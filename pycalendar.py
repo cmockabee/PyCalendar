8f08a8f83137d77bf51cd0e6d390d54bb54e82b4
cmockabee <cmock918@gmail.com>
2017-11-30 18:51:56 +0100

    now it adds tarea to the closest week it is in. format is yyyy-mm-dd

--- pycalendar.py
+++ pycalendar.py
@@ -50,26 +50,15 @@ tree.heading("two", text="Class")
 tree.heading("three", text="Assignment")
 tree.heading("four", text="extra")
 
-
-
-#this will go in the menu function
-testInsert1 = tree.insert("", 0, "Test1", text="Test1")
-tree.insert(testInsert1, "end", "Test 1", text="SubTest1", values=("A","B","C","D"))
-#this will go in the menu function
-testInsert2 = tree.insert("", 0, "Test2", text="Test2")
-tree.insert(testInsert2, "end", "Test 2", text="SubTest2", values=("A","B","C","D"))
-tree.insert(testInsert2, "end", "Test 2.1", text="SubTest3", values=("A","B","C","D"))
-
-
+# This creates 6 week view starting from closest past Sunday
 for x in range (1, 9):
 	tree.insert("", 0, closestSundayDate, text=closestSundayDate)
 	tree.insert(closestSundayDate, 0, text="Date Due",values=("x", "x","x","x"))
 	closestSundayDate += one_week
-
-
-
+	
 tree.pack()
-  
+
+
 
 #------------------------------------------
 # Menu Entry
@@ -79,15 +68,13 @@ master = Tk()
 
 while menuBool == True:   
 	
-	def menu():
-		
-		def show_entry_fields():
-			print("Assignment Name: %s\nDate Due: %s\nClass Code: %s" % (e1.get(), e2.get(), e3.get()))
-			
+	def menu():		
 		
 		def insert_assignment():
 			assignmentDate = datetime.datetime.strptime(e2.get(), "%Y-%m-%d").date()
-			tree.insert(assignmentDate, 0, text=e2.get(),values=(e1.get(), e2.get(),e3.get(),"1b"))
+			closestSundayTarea = assignmentDate - (datetime.timedelta(days= (assignmentDate.weekday() + 1)))
+			
+			tree.insert(closestSundayTarea, 0, text=assignmentDate,values=(e1.get(), e2.get(),e3.get(),"1b"))
 			obj = Tarea(e1.get(),e2.get(),e3.get())
 			tareaList.append(obj)
 			menu()
@@ -96,30 +83,27 @@ while menuBool == True:
 		Label(master, text="Assignment Name").grid(row=0)
 		Label(master, text="Date Due").grid(row=1)
 		Label(master, text="Class Code").grid(row=2)
-
+		
+		# Inputs
 		e1 = Entry(master)
 		e1.focus_set()
 		e2 = Entry(master)
 		e3 = Entry(master)
-		
 
 		e1.grid(row=0, column=1)
 		e2.grid(row=1, column=1)
 		e3.grid(row=2, column=1)
 
-		
+		# Quit Program Button
+		# Insert Assignment Button
 		btn1 = Button(master, text='Quit', command=quit).grid(row=3, column=0, sticky=W, pady=4)
-		btn2 = Button(master, text='Insert Class', command=insert_assignment).grid(row=3, column=1, sticky=W, pady=4)
-		
+		btn2 = Button(master, text='Insert Class', command=insert_assignment).grid(row=3, column=1, sticky=W, pady=4)	
 	
 	menu()
 	
 	menuBool = False
 
-master.mainloop( )
-
-   
-   
+master.mainloop( )  
 
 root.mainloop()
 
