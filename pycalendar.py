0fc67a0fcd161da68a55df4b267b1aed76555a9c
cmockabee <cmock918@gmail.com>
2017-12-07 17:55:19 +0100

    finally got dates to work with pickle

--- pycalendar.py
+++ pycalendar.py
@@ -9,6 +9,7 @@ import pickle
 import os
 
 
+
 today = datetime.date.today()
 closestSunday = datetime.timedelta(days=(datetime.date.today().weekday() + 1))
 closestSundayDate = today - closestSunday
@@ -22,40 +23,14 @@ one_week = datetime.timedelta(days=7)
 # Tarea Creation
 #------------------------------------------
 
-tareaList = []
-
-
 class Tarea:
 	tareaCount = 0
 	# assignment name, date due, and class code
 	def __init__(self, name, datedue, code):
 		self.name = name
 		self.datedue = datedue
+		self.code = code
 		Tarea.tareaCount += 1
-
-
-
-# pickle_out = open("list.pickle", "wb")
-# pickle.dump(tareaList, pickle_out)
-# pickle_out.close()
-
-
-#------------------------------------------
-# Load List With Pickle
-#------------------------------------------
-
-
-
-
-if os.path.getsize("list.pickle") > 0:      
-    with open("list.pickle", "rb") as f:
-        unpickler = pickle.Unpickler(f)
-        # if file is not empty scores will be equal
-        # to the value unpickled
-        tareaList = unpickler.load()
-        f.close()
-
-print(tareaList)
    
 
 #------------------------------------------
@@ -65,35 +40,54 @@ print(tareaList)
 root = Tk()
 tree = ttk.Treeview(root)
 
-tree["columns"]=("one","two","three","four")
+tree["columns"]=("one","two","three")
 tree.column("one", width=100 )
 tree.column("two", width=100)
 tree.column("three", width=100 )
-tree.column("four", width=100)
+
 tree.heading("one", text="Date Due")
 tree.heading("two", text="Class")
 tree.heading("three", text="Assignment")
-tree.heading("four", text="extra")
+
 
 # This creates 6 week view starting from closest past Sunday
 for x in range (1, 9):
 	tree.insert("", 0, closestSundayDate, text=closestSundayDate)
-	tree.insert(closestSundayDate, 0, text="Date Due",values=("x", "x","x","x"))
+	tree.insert(closestSundayDate, 0, text="Date Due",values=("x", "x","x"))
 	closestSundayDate += one_week
 
-
 x = 0
-for Tarea in tareaList:
-	
-	assignmentDate = datetime.datetime.strptime(tareaList[x].datedue, "%Y-%m-%d").date()
-	closestSundayTarea = assignmentDate - (datetime.timedelta(days= (assignmentDate.weekday() + 1)))
-	tree.insert(closestSundayTarea, 0, text=tareaList[x].datedue,values=(tareaList[x].name, tareaList[x].name,tareaList[x].name,"1b"))
-	x += 1
-
-
 tree.pack()
 
 
+#------------------------------------------
+# Pickle Load
+#------------------------------------------
+tareaList = []
+
+
+try:
+	with (open('list.pkl', 'rb')) as openfile:
+		while True:
+			try:
+				obj = pickle.load(openfile)
+				tareaList.append(obj)
+				print(obj.datedue)
+			except EOFError:
+				break
+except (OSError, IOError) as e:
+	with open('list.pkl', 'wb') as file:
+		pickle.dump(tareaList, file, pickle.HIGHEST_PROTOCOL)
+	
+
+
+# a = 0
+# for obj in enumerate(tareaList):
+	# closestSundayTarea = tareaList[a].datedue - (datetime.timedelta(days= (tareaList[a].datedue.weekday() + 1)))
+	# tree.insert(closestSundayTarea, 0, text=tareaList[a].datedue,values=(tareaList[a].name, tareaList[a].datedue, tareaList[a].code))
+	# print(obj)
+	# a+=1
+	
 
 #------------------------------------------
 # Menu Entry
@@ -109,13 +103,10 @@ while menuBool == True:
 			assignmentDate = datetime.datetime.strptime(e2.get(), "%Y-%m-%d").date()
 			closestSundayTarea = assignmentDate - (datetime.timedelta(days= (assignmentDate.weekday() + 1)))
 			
-			tree.insert(closestSundayTarea, 0, text=assignmentDate,values=(e1.get(), e2.get(),e3.get(),"1b"))
+			tree.insert(closestSundayTarea, 0, text=assignmentDate,values=(e1.get(), e2.get(),e3.get()))
 			obj = Tarea(e1.get(),e2.get(),e3.get())
 			tareaList.append(obj)
-
-			print(obj.name)
-			print(tareaList[0].name)
-
+			
 			menu()	
 
 		
@@ -142,11 +133,20 @@ while menuBool == True:
 	menu()
 	menuBool = False
 
+	
+
 master.mainloop( )  
 root.mainloop()
 
-pickle_out = open("list.pickle", "wb")
-pickle.dump(tareaList, pickle_out)
-pickle_out.close()
+with open('list.pkl', 'wb') as file:
+	for obj in tareaList:
+		pickle.dump(obj, file, pickle.HIGHEST_PROTOCOL)
+
+print(tareaList)
+
+
+
+
+
 
 
